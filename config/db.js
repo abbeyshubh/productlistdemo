5071ac3716b93520aa7b3c5d0b4eea6c35ea9e4a
abbeyshubh <shubhamabbey54@gmail.com>
2020-07-11 19:27:48 +0530

    heroku1

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 //mongodb+srv://abbey7991:shubh1997@cluster0-4lzfj.mongodb.net/abbey7991?retryWrites=true&w=majority
 mongoose
   .connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/categoriesDB",
+    process.env.MONGODB_URI || "mongodb://localhost:27017/shoppingCart",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
